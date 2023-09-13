3dff8af27acb9ed830b0ec8c92ce5a7772d18a9a
jhokk <jayhokkanen25@gmail.com>
2023-09-13 01:09:57 -0400

    Testing8

--- main.test.js
+++ main.test.js
@@ -2,14 +2,22 @@ import { default as seagulls } from './seagulls.js'
 
 const shader = `${seagulls.constants.vertex}
 
-@fragment 
-fn fs( @builtin(position) pos : vec4f ) -> @location(0) vec4f {;
-  return vec4f( 1.,0.,0. , 1. );
+@group(0) @binding(0) var <uniform> frame : f32;
+@group(0) @binding(1) var <uniform> resolution : vec2f;
+
+@fragment
+fn fs( @builtin(position) pos : vec4f ) -> @location(0) vec4f {
+  let uv = pos.xy / resolution;
+  let time = frame / 60.;
+  return vec4f( uv.x, uv.y, time % 1., 1. );
 }`
 
-async function main() {
-  const sg = await seagulls.init()
-  sg.render( shader ).run()
-}
+const sg         = await seagulls.init()
+const resolution = [ window.innerWidth, window.innerHeight ]
+let frame = 0
 
-main()
+sg
+  .uniforms({ frame, resolution })
+  .onframe( ()=> sg.uniforms.frame = frame++ ) 
+  .render( shader )
+  .run()
